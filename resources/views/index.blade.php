e366cc1f2e5364c2bc8e72cb185beee81234d061
David Schiro <davidschiro91@gmail.com>
2021-02-20 22:15:49 -0300

    Create Box Infos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Soho CX & UX · Consultoría y Desarrollo de Proyectos</title>
+
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+
+    <body>
+        <div class="container_box">
+            <section class="box_info box_1">
+                <h3>Estrategía, usabilidad y ux</h3>
+                <p>
+                Te ofrecemos excelente usabilidad y experiencia del usuario en tu proyecto, junto a una visión innovadora
+                </p>
+            </section>
+            <section class="box_info box_2">
+                <h3>Mejoramos la experiencia</h3>
+                <p>
+                Tangibilizamos tu proyecto digital a través de un diseño centrado en el usuario y técnicas avanzadas
+                </p>
+            </section>
+            <section class="box_info box_3">
+                <h3>Medición continua de objetivos</h3>
+                <p>
+                Implementación, testig con usuarios y medición continua son necesarias para garantizar el exito de tu proyecto
+                </p>
+            </section>
+        </div>
+    </body>
+<html>
