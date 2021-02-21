cb775f34e56af53858b9e91c3119b3ed593eb88f
David Schiro <davidschiro91@gmail.com>
2021-02-21 01:49:57 -0300

    Header, Title and subtitles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,41 @@
         <title>Soho CX & UX · Consultoría y Desarrollo de Proyectos</title>
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
 
     <body>
+        <header>
+            <nav>
+                <div class="logo m-15"><img src="{{asset('images/logo-soho.png')}}" alt="Logo Soho"></div>
+                
+                <ul class="m-15">
+                    <li class="selected"><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Casos de éxito</a></li>
+                    <li><a href="#">Únete al equipo</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+            <section class="center d-small-none">
+                <h3 class="my-5">Nos especializamos en</h3>
+                <h2 class="text-accent my-5">interfaces digitales que los usuarios aman</h2>
+                <a href="#" class="button m-15">Hablemos de tu proyecto</a>
+
+            </section>
+            <section class="footer d-small-none">
+                <i class="material-icons text-danger">sensors</i> <b>EN DIRECTO</b> - SOHO está presente en el <b>DIGITALBANK MONTEVIDEO</b>. "Experiencia de Usuario" por Alvaro Añón (SEO de Soho).
+                <a href="#" class="button mx-10">Ver evento</a>
+                <a href="#" class="button transparent transparent">Próximos eventos</a>
+            </section>
+        </header>
+
+        <section class="d-flex flex-flow-c w-100 text-center jc-center align-center"> 
+            <h1 class="titulo w-100">17 años de experiencia en pos de tu proyecto</h1>
+            <p class="subtitulo">Especializados desde 1996 en usabilidad, experiencia del usuario (UX) y diseño de experiencias de proyectos digitales. Aportamos estrategia e innovación centrada en el usuario y los objetivos de negocio de tu proyecto. Cotrabajando mejoraremos tu tasa conversión, KPI's, ROI y los resultados de la actual publicadad PPC (pay per click)</p>
+        </section>
+
         <div class="container_box">
             <section class="box_info box_1">
                 <h3>Estrategía, usabilidad y ux</h3>
@@ -30,5 +62,6 @@
                 </p>
             </section>
         </div>
+
     </body>
 <html>
